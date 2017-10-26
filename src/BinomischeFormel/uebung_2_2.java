76daaa23b1d20ca89201231781657e9ecd1479b3
15526 <15526@A001-18.nordakademie.intern>
2017-10-26 11:48:17 +0200

    Übung 2_2

---
+++ src/BinomischeFormel/uebung_2_2.java
@@ -0,0 +1,18 @@
+package BinomischeFormel;
+
+public class uebung_2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double a = 8;
+double b = 3;
+
+double links = ((a+b)*(a+b));
+double rechts = ((a*a)+2*a*b+(b*b));
+
+System.out.println("links"+links);
+System.out.println("rechts"+rechts);		
+		
+	}
+
+}
